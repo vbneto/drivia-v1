c45243af2e2989dfa41d54da0337e4daa2962525
ritesh-ror2 <ritesh.ror@gmail.com>
2014-04-02 20:37:55 +0530

    minor view changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def school_administration_for_student_dashboard
+    "Loged is as School Administration" if controller.controller_name == 'users' and controller.action_name == 'index' 
+  end
+  
 end
