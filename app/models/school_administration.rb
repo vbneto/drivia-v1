f1593c0103c04654fe07af8169d6ba2065466cf7
ritesh-ror2 <ritesh.ror@gmail.com>
2014-04-03 12:05:41 +0530

    search student by name and show parent details

--- app/models/school_administration.rb
+++ app/models/school_administration.rb
@@ -3,4 +3,13 @@ class SchoolAdministration < ActiveRecord::Base
   has_one :user
   has_many :student_from_excels, :through => :school
   belongs_to :school
+  
+  def find_students (student)
+    if student.blank?
+      self.student_from_excels
+    else
+      self.student_from_excels.where("student_name LIKE ?" , "%#{student}%")
+    end
+  end
+
 end
